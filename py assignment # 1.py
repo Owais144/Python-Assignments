9b6d3c271dddca058ee9048985e995f9f5723cf4
Muhammad Owais <muhammadowais191@gmail.com>
2019-11-02 20:59:53 +0500

    Python Programs

---
+++ py assignment # 1.py
@@ -0,0 +1,38 @@
+#Q1
+#print("Twinkle, twinkle, little star,\n\tHow I wonder what you are!\n\t\t\tUp above the world so high\n\t\t\tLike a diamond in the sky.\nTwinkle, twinkle, little star,\n\tHow I wonder what you are");
+
+#Q2
+"""
+import sys
+print("Python version")
+print (sys.version)
+"""
+
+#Q3
+"""
+import datetime
+datetime_ob = datetime.datetime.today()
+print(datetime_ob)
+"""
+#Q4
+"""
+r =float(input(" Enter radius of the circle = "))
+Area = 3.14*r**2
+print("Area of the circle is : %s" %Area)
+"""
+#Q5
+"""
+fn=input("Enter your first name = ")
+ln=input("Enter your last name = ")
+print("Name in reverse = ",ln + " " + fn)
+"""
+
+#Q6
+"""
+one= int(input("Enter first number = "))
+two= int(input("Enter second number = "))
+output= one + two
+print("Result =",output)
+"""
+
+
